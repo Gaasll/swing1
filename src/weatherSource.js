3cc87271e5ee34d59a09186fec0baae4eedab6f8
weeping nazgûl <antongor@kth.se>
2022-12-06 13:35:13 +0100

    weathersrc has a getter function

--- src/weatherSource.js
+++ src/weatherSource.js
@@ -1,6 +1,21 @@
 import { BASE_URL_WEATHER, API_KEY_WEATHER } from "./apiConfig.js";
 
-function getCurrentWeather(params){
+
+const lat = 59.40; 
+const lon = 17.95;
+
+function treatHTTPResponseACB(response){ 
+    if(response.status !== 200){throw new Error("API problem " + response.status); }
+    return response.json();
+}
+
+function getCurrentWeather(){
+    return fetch(BASE_URL_WEATHER+lat+'&'+lon+'&appid='+ API_KEY_WEATHER,{ 
+    "method": "GET"
+        }
+    ).then(treatHTTPResponseACB)
 
 
 }
+
+export {getCurrentWeather};
